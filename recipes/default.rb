fdb7ca6ff837ff72c073f7d98e7945f4eeb771d5
Chia-liang Kao <clkao@clkao.org>
2013-06-12 23:58:56 +0800

    add pgextwlist extension and allow plv8

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,8 @@
+include_recipe 'postgresql::server'
+
+node.override['postgresql']['config']['local_preload_libraries'] = 'pgextwlist.so'
+node.override['postgresql']['config']['extwlist.extensions'] = 'plv8'
+
 package "python-pip" do
   action :install
 end
@@ -17,4 +22,18 @@ end
 execute "install pgrest" do
   command "npm i -g pgrest"
   not_if "test -e /usr/bin/pgrest"
+end
+
+script "install pgextwlist" do
+  interpreter "bash"
+  user "root"
+  cwd "/root"
+  code <<-EOH
+    git clone git://github.com/dimitri/pgextwlist.git
+    cd pgextwlist
+    make
+    mkdir /usr/lib/postgresql/#{ node['postgresql']['version'] }/lib/plugins
+    cp -f ./pgextwlist.so /usr/lib/postgresql/#{ node['postgresql']['version'] }/lib/plugins/
+  EOH
+  not_if "test -e /usr/share/postgresql/#{ node['postgresql']['version'] }/lib/plugins/pgextwlist.so"
 end
